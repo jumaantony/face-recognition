897706cf01df24315c9a346701a6b319084198bb
Juma Antony <jumaantony840@gmail.com>
2023-03-06 08:36:16 +0300

    added user rank

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+        <div className='white f5'>
+            {'Hello Juma, your Current Rank is ...'}
+        </div>
+
+        <div className='white f2'>
+            {'#5'}
+        </div>
+      
+    </div>
+  )
+}
+
+export default Rank;

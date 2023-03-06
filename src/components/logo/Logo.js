2b49bb94e18e3ab2d03e4c0570e1c47df9c4bfc9
Juma Antony <jumaantony840@gmail.com>
2023-03-06 07:28:14 +0300

    added my navigation and logo

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Logo.css'
+import brain from './brain.png'
+
+import Tilt from 'react-parallax-tilt';
+
+const Logo = () => {
+  return (
+    <div className='ma4 mt0'>
+        <Tilt className='tilt br2 shadow-2' style={{ height: 80, width:80}}>
+            <div className='titl-inner'>
+                <img src={brain} alt='logo'  style={{paddingtop: 5px;}}/>
+            </div>
+        </Tilt>
+    </div>
+  )
+}
+
+export default Logo;

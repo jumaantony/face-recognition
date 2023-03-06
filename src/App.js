8f27d48934ae6f0f9f224738dfb47d3c69f247bb
Juma Antony <jumaantony840@gmail.com>
2023-03-06 07:53:11 +0300

    created image link form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* 
       <ImageLinkForm />
+      {/* 
+      
       <FaceRecognition /> */}
     </div>
   );

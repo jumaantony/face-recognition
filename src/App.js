485a685a75fa2ee0b391c2e575c4919a05983432
Juma Antony <jumaantony840@gmail.com>
2023-03-07 12:42:02 +0300

    created face detection using clarify

--- src/App.js
+++ src/App.js
@@ -1,27 +1,122 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank'
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
-import ParticlesBg from 'particles-bg'
+import ParticlesBg from 'particles-bg';
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg type="cobweb" bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* 
-      
-      <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  // function to calculate face location
+
+  calculateFaceLocation = (data) => {
+    const clarifyFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage')
+    const width = Number(image.width)
+    const height =Number(image.height)
+    console.log(clarifyFace)
+    console.log(width, height)
+
+    return{
+      leftCol: clarifyFace.left_col * width,
+      topRow: clarifyFace.top_row * height,
+      rightCol: width - (clarifyFace.right_col * width),
+      bottomRow: height - (clarifyFace.bottom_row * height),
+    }
+  }
+
+  //function to display facebox
+
+  displayFaceBox = (box) => {
+    console.log(box)
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit =() => {
+    this.setState({imageUrl: this.state.input});
+
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = '040b1307327f4701912a18eed0e36be3';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'wicked';       
+    const APP_ID = 'facedect';
+    // Change these to whatever model and image URL you want to use
+    const MODEL_ID = 'face-detection';
+    const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
+    const IMAGE_URL = this.state.input;
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+
+  // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+  // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+  // this will default to the latest version_id
+
+
+  fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
+      .then(response => response.json())
+
+      .then(result => {
+        this.displayFaceBox(this.calculateFaceLocation(result))
+
+      })
+
+      .catch(error => console.log('error', error))
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <ParticlesBg type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <FaceRecognition imageUrl={this.state.imageUrl}  box={this.state.box} />
+      </div>
+    );
+  }
 }
 
 export default App;
